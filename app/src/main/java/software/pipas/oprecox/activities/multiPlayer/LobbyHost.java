f302764587ef13b931b10f90f0de95b2c4e64f0a
uno <nuno_mgl@hotmail.com>
2017-06-30 21:09:17 +0100

    updated layout in multiplayer lobby host

--- app/src/main/java/software/pipas/oprecox/activities/multiPlayer/LobbyHost.java
+++ app/src/main/java/software/pipas/oprecox/activities/multiPlayer/LobbyHost.java
@@ -3,6 +3,7 @@ package software.pipas.oprecox.activities.multiPlayer;
 import android.os.Bundle;
 
 
+import software.pipas.oprecox.R;
 import software.pipas.oprecox.modules.customActivities.MultiplayerClass;
 
 public class LobbyHost extends MultiplayerClass
@@ -12,6 +13,7 @@ public class LobbyHost extends MultiplayerClass
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_multiplayer_lobby_host);
     }
 
 
